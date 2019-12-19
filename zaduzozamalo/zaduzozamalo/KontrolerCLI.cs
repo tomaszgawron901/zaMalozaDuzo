69a6d3614fda5bc554df3c6d5c558fa5d29b8ef3
tomaszgawron901 <49018724+tomaszgawron901@users.noreply.github.com>
2019-12-19 18:27:27 +0100

    Add files via upload

---
+++ zaduzozamalo/zaduzozamalo/KontrolerCLI.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using GraZaDuzoZaMalo.Model;
+using static GraZaDuzoZaMalo.Model.Gra.Odpowiedz;
+
+namespace AppGraZaDuzoZaMaloCLI
+{
+    public class KontrolerCLI
+    {
+        public const char ZNAK_ZAKONCZENIA_GRY = 'X';
+
+        private Gra gra;
+        private WidokCLI widok;
+
+        public int MinZakres { get; private set; } = 1;
+        public int MaxZakres { get; private set; } = 100;
+
+        public IReadOnlyList<Gra.Ruch> ListaRuchow {
+            get
+            { return gra.ListaRuchow;  }
+ }
+
+        public KontrolerCLI()
+        {
+            gra = new Gra();
+            widok = new WidokCLI(this);
+        }
+
+        public void Uruchom()
+        {
+            widok.OpisGry();
+            while( widok.ChceszKontynuowac("Czy chcesz kontynuować aplikację (t/n)? ") )
+                UruchomRozgrywke();
+        }
+
+        public void UruchomRozgrywke()
+        {
+            widok.CzyscEkran();
+            // ustaw zakres do losowania
+
+
+            gra = new Gra(MinZakres, MaxZakres); //może zgłosić ArgumentException
+
+            do
+            {
+                //wczytaj propozycję
+                int propozycja = 0;
+                try
+                {
+                    propozycja = widok.WczytajPropozycje();
+                }
+                catch( KoniecGryException)
+                {
+                    gra.Przerwij();
+                }
+
+                Console.WriteLine(propozycja);
+
+                if (gra.StatusGry == Gra.Status.Poddana) break;
+
+                //Console.WriteLine( gra.Ocena(propozycja) );
+                //oceń propozycję, break
+                switch( gra.Ocena(propozycja) )
+                {
+                    case ZaDuzo:
+                        widok.KomunikatZaDuzo();
+                        break;
+                    case ZaMalo:
+                        widok.KomunikatZaMalo();
+                        break;
+                    case Trafiony:
+                        widok.KomunikatTrafiono();
+                        break;
+                    default:
+                        break;
+                }
+                widok.HistoriaGry();
+            }
+            while (gra.StatusGry == Gra.Status.WTrakcie);
+                      
+            //if StatusGry == Przerwana wypisz poprawną odpowiedź
+            //if StatusGry == Zakończona wypisz statystyki gry
+        }
+
+        ///////////////////////
+
+        public void UstawZakresDoLosowania(ref int min, ref int max)
+        {
+
+        }
+
+        public int LiczbaProb() => gra.ListaRuchow.Count();
+
+        public void ZakonczGre()
+        {
+            IFormatter formater = new BinaryFormatter();
+            Stream stream = new FileStream(@"save.txt", FileMode.Create, FileAccess.Write);
+            formater.Serialize(stream, gra);
+            stream.Close();
+            gra = null;
+            widok.CzyscEkran(); //komunikat o końcu gry
+            widok = null;
+            System.Environment.Exit(0);
+        }
+
+        public void ZakonczRozgrywke()
+        {
+            gra.Przerwij();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defaultValue"></param>
+        /// <exception cref="KoniecGryException"></exception>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="OverflowException"></exception>
+        /// <returns></returns>
+        public int WczytajLiczbeLubKoniec(string value, int defaultValue )
+        {
+            if( string.IsNullOrEmpty(value) )
+                return defaultValue;
+
+            value = value.TrimStart().ToUpper();
+            if (value.Length > 0 && value[0].Equals(ZNAK_ZAKONCZENIA_GRY))
+                ZakonczGre();
+
+            //UWAGA: ponizej może zostać zgłoszony wyjątek 
+            return Int32.Parse(value);
+        }
+    }
+
+    [Serializable]
+    internal class KoniecGryException : Exception
+    {
+        public KoniecGryException()
+        {
+        }
+
+        public KoniecGryException(string message) : base(message)
+        {
+        }
+
+        public KoniecGryException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected KoniecGryException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
